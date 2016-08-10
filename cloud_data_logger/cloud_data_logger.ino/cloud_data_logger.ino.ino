04ccc4642a52e21c00ef1e48272c7cbc6525e1ac
Ari Volcoff <ariv@il.ibm.com>
2016-08-10 16:12:54 +0300

    added support for dust sensor

--- cloud_data_logger/cloud_data_logger.ino/cloud_data_logger.ino.ino
+++ cloud_data_logger/cloud_data_logger.ino/cloud_data_logger.ino.ino
@@ -1,47 +1,106 @@
-// Import required libraries
-#include "ESP8266WiFi.h"
-#include "DHT.h"
-#include <QueueArray.h>
+#include <Mail.h>
+#include <SmtpClient.h>
 
-// WiFi parameters
-const char* ssid = "birds";
-const char* password = "ceardida";
+/**********************************************/
+/* Building Arduino Dust Sensor using:        */
+/*      - ESP8266 ESP-01                      */
+/*      - 3.3-to-5v Logic Level Converter     */
+/*      - Shinyei PPD42NS                     */
+/* http://www.sca-shinyei.com/pdf/PPD42NS.pdf */
+/*                                            */
+/* Author: shadowandy[dot]sg[at]gmail[dot]com */
+/* Web: www.shadowandy.net                    */
+/*                                            */         
+/* Wiring Instruction:                        */
+/*      - PPD42NS Pin 1 => GND                */
+/*      - PPD42NS Pin 2 => GPIO0              */
+/*      - PPD42NS Pin 3 => 5V                 */
+/*      - PPD42NS Pin 4 => GPIO2              */
+/**********************************************/
 
-//String airQualityHistory = "";
+#include <ESP8266WiFi.h>               
 
-//Sensor Setup
+const char ssid[] = "<YOUR WIFI SSID NAME>";
+const char pass[] = "<YOUR WIFI PASSWORD>";
+const char thingSpeakAddress[] = "api.thingspeak.com";
+const char thingSpeakAPIKey[] = "<YOUR THINGSPEAK KEY>";
+const char thingSpeakTwilioAPIKey[] = "<YOUR TWILIO KEY>";
+
+int pin = 0;
+unsigned long duration;
+unsigned long starttime;
+unsigned long sampletime_ms = 2000;//sampe 30s&nbsp;;
+unsigned long lowpulseoccupancy = 0;
+float ratio = 0;
+float concentration = 0;
+
+// sensor setup
 #define airquality_sensor_pin 0
 #define gas_sensor_pin 1
 
-// Pin
-#define DHTPIN 5
-
-// Use DHT11 sensor
-#define DHTTYPE DHT11
-
-// Initialize DHT sensor
-DHT dht(DHTPIN, DHTTYPE, 15);
+//the number the message should be sent to
+const String sendNumber = "+972584168357";
 
 // Host
-const char* host = "dweet.io";
+const char* dweetHost = "dweet.io";
 
+char server[] = "mail.smtp2go.com";
+int port = 2525;
 
-  
+String message = "";
+
+WiFiClient client;
+
 void setup() {
-  
-  // Start Serial
   Serial.begin(115200);
-  delay(10);
-  
-  // Init DHT 
-  dht.begin();
+  connectWiFi();
+  pinMode(pin,INPUT);
+  starttime = millis();//get the current time;
+}
 
-  // We start by connecting to a WiFi network
+void loop() {
+  duration = pulseIn(pin, LOW);
+  lowpulseoccupancy = lowpulseoccupancy+duration;
+ 
+  if ((millis()-starttime) >= sampletime_ms)//if the sampel time = = 30s
+  {
+    ratio = lowpulseoccupancy/(sampletime_ms*10.0);  // Integer percentage 0=&gt;100
+    concentration = 1.1*pow(ratio,3)-3.8*pow(ratio,2)+520*ratio+0.62; // using spec sheet curve
+    Serial.print("concentration = ");
+    Serial.print(concentration);
+    Serial.println(" pcs/0.01cf");
+    Serial.println("\n");
+    lowpulseoccupancy = 0;
+    starttime = millis();
+  }
+    connectWiFi();
+
+     // This will send the request to the server
+  int airquality_value = analogRead(airquality_sensor_pin);
+    int gas_value = analogRead(gas_sensor_pin);
+    float volume = (float)gas_value/1024*5.0*1000;
+
+    message = String("GET /dweet/for/ari-gas-sensor?concentration=") + String(concentration) + "&airquality=" + String(airquality_value) + " HTTP/1.1\r\n" +
+               "Host: " + dweetHost + "\r\n" + 
+               "Connection: close\r\n\r\n";
+    sendDataToFreeBoard(message);
+    
+    message = "1=" + String(concentration, DEC) + "&2=" + String(airquality_value, DEC);
+    updateThingSpeak(message);
+
+    if (concentration > 5000 || airquality_value > 70){
+      sendAlert("Please open the window : Dust Concentration="+String(concentration,DEC)+ " ,Air Quality Level="+String(airquality_value,DEC));
+      delay(900000);
+    }
+}
+
+void connectWiFi() {
+// We start by connecting to a WiFi network
   Serial.println();
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(ssid);
-  WiFi.begin(ssid, password);
+  WiFi.begin(ssid, pass);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
@@ -51,36 +110,49 @@ void setup() {
   Serial.println("WiFi connected");  
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
- 
 }
 
-void loop() {
- 
-  Serial.print("Connecting to ");
-  Serial.println(host);
+void updateThingSpeak(String tsData) {
+  Serial.println(tsData);
   
-  // Use WiFiClient class to create TCP connections
-  WiFiClient client;
-  const int httpPort = 80;
-  if (!client.connect(host, httpPort)) {
-    Serial.println("connection failed");
+  if (!client.connect(thingSpeakAddress, 80)) {
     return;
   }
-  
-  // This will send the request to the server
-  int airquality_value = analogRead(airquality_sensor_pin);
-  /*if (airQualityHistory.length()==0)
-    airQualityHistory = "[" + String(airquality_value);
-  else
-    airQualityHistory = airQualityHistory + "," + String(airquality_value);  */
-  
-  int gas_value = analogRead(gas_sensor_pin);
-  float volume = (float)gas_value/1024*5.0*1000;
- 
-  delay(50);
-  String message = String("GET /dweet/for/arisgassensor?airquality_value=") + String(airquality_value) + "&volume=" + String(volume) + " HTTP/1.1\r\n" +
-               "Host: " + host + "\r\n" + 
-               "Connection: close\r\n\r\n";
+  client.print(F("GET /update?key="));
+  client.print(thingSpeakAPIKey);
+  client.print(F("&"));
+  client.print(tsData);
+  client.print(F(" HTTP/1.1\r\nHost: api.thingspeak.com\r\n\r\n"));
+  client.println();
+}
+
+void sendAlert(String message){
+  sendEmail(message);
+}
+
+String URLEncode(const char* msg)
+{
+  const char *hex = "0123456789abcdef";
+  String encodedMsg = "";
+
+  while (*msg!='\0'){
+    if( ('a' <= *msg && *msg <= 'z')
+      || ('A' <= *msg && *msg <= 'Z')
+      || ('0' <= *msg && *msg <= '9') ) {
+      encodedMsg += *msg;
+    } 
+    else {
+      encodedMsg += '%';
+      encodedMsg += hex[*msg >> 4];
+      encodedMsg += hex[*msg & 15];
+    }
+    msg++;
+  }
+  return encodedMsg;
+}
+
+void sendDataToFreeBoard(String message){
+  Serial.println("inside sendDataToFreeBoard");
   client.print(message);
   Serial.println(message);
   delay(10);
@@ -93,11 +165,146 @@ void loop() {
   
   Serial.println();
   Serial.println("closing connection");
-
-
-  
-  // Repeat every 10 seconds
-  delay(1000);
- 
 }
 
+byte sendEmail(String message)
+{
+  byte thisByte = 0;
+  byte respCode;
+ 
+  if(client.connect(server,port) == 1) {
+    Serial.println(F("connected"));
+  } else {
+    Serial.println(F("connection failed"));
+    return 0;
+  }
+ 
+  if(!eRcv()) return 0;
+ 
+  Serial.println(F("Sending hello"));
+// replace 1.2.3.4 with your Arduino's ip
+  client.println("EHLO 192.168.1.5");
+  if(!eRcv()) return 0;
+ 
+  Serial.println(F("Sending auth login"));
+  client.println("auth login");
+  if(!eRcv()) return 0;
+ 
+  Serial.println(F("Sending User"));
+// Change to your base64 encoded user
+  client.println("YXZvbGNvZmY=");
+ 
+  if(!eRcv()) return 0;
+ 
+  Serial.println(F("Sending Password"));
+// change to your base64 encoded password
+  client.println("Y2VhcmRpZGE=");
+ 
+  if(!eRcv()) return 0;
+ 
+// change to your email address (sender)
+  Serial.println(F("Sending From"));
+  client.println("MAIL From: avolcoff@gmail.com");
+  if(!eRcv()) return 0;
+ 
+// change to recipient address
+  Serial.println(F("Sending To"));
+  client.println("RCPT To: avolcoff@gmail.com");
+  if(!eRcv()) return 0;
+ 
+  Serial.println(F("Sending DATA"));
+  client.println("DATA");
+  if(!eRcv()) return 0;
+ 
+  Serial.println(F("Sending email"));
+ 
+// change to recipient address
+  client.println("To: You avolcoff@gmail.com");
+ 
+// change to your address
+  client.println("From: Me avolcoff@gmail.com");
+ 
+  client.println("Subject: Air Quality Alert\r\n");
+ 
+  client.println(message);
+ 
+  client.println(".");
+ 
+  if(!eRcv()) return 0;
+ 
+  Serial.println(F("Sending QUIT"));
+  client.println("QUIT");
+  if(!eRcv()) return 0;
+ 
+  client.stop();
+ 
+  Serial.println(F("disconnected"));
+ 
+  return 1;
+}
+ 
+byte eRcv()
+{
+  byte respCode;
+  byte thisByte;
+  int loopCount = 0;
+ 
+  while(!client.available()) {
+    delay(1);
+    loopCount++;
+ 
+    // if nothing received for 10 seconds, timeout
+    if(loopCount > 10000) {
+      client.stop();
+      Serial.println(F("\r\nTimeout"));
+      return 0;
+    }
+  }
+ 
+  respCode = client.peek();
+ 
+  while(client.available())
+  {  
+    thisByte = client.read();    
+    Serial.write(thisByte);
+  }
+ 
+  if(respCode >= '4')
+  {
+    efail();
+    return 0;  
+  }
+ 
+  return 1;
+}
+ 
+ 
+void efail()
+{
+  byte thisByte = 0;
+  int loopCount = 0;
+ 
+  client.println(F("QUIT"));
+ 
+  while(!client.available()) {
+    delay(1);
+    loopCount++;
+ 
+    // if nothing received for 10 seconds, timeout
+    if(loopCount > 10000) {
+      client.stop();
+      Serial.println(F("\r\nTimeout"));
+      return;
+    }
+  }
+ 
+  while(client.available())
+  {  
+    thisByte = client.read();    
+    Serial.write(thisByte);
+  }
+ 
+  client.stop();
+ 
+  Serial.println(F("disconnected"));
+}
